6a53f04b1546e7e91a6b6d26f5a651bb5028e9d1
Hannah Mihulka <hannahmihulka@gmail.com>
2023-07-24 19:07:35 -0500

    initiating git request

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// TODO: add code here
+window.addEventListener('load', function () {
+    const container = document.getElementById('container');
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+.then(Response => Response.json()) 
+.then(data => {
+    data.forEach(astronaut => {
+        const astronautCard = createAstronautCard(astronaut);
+        container.appendChild(astronautCard);
+    });
+}); 
+.catch(error => {
+    console.error('Error fetching astronauts', error);
+});
+}); 
