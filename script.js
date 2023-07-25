b35898248958e8cd3f32ef9598b4414af71893da
Hannah Mihulka <hannahmihulka@gmail.com>
2023-07-25 13:29:33 -0500

    initial fetch from api

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', function () {
     const container = document.getElementById('container');
-    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
-        
-    }
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(response => response.json()) 
+    .then (data => {
+        console.log(data)
+    })
+}); 
