ad72a7b4ae02a2b86e13a7e7cefa3971a8df243b
Hannah Mihulka <hannahmihulka@gmail.com>
2023-07-24 19:54:09 -0500

    fetch

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 window.addEventListener('load', function () {
     const container = document.getElementById('container');
-    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
-.then(Response => Response.json()) 
-.then(data => {
-    data.forEach(astronaut => {
-        const astronautCard = createAstronautCard(astronaut);
-        container.appendChild(astronautCard);
-    });
-}); 
-.catch(error => {
-    console.error('Error fetching astronauts', error);
-});
-}); 
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+        
+    }
